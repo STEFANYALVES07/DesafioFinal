e64f9f4898f21dc317a3508453bb258c8b344dd0
Stefany <125682393+STEFANYALVES07@users.noreply.github.com>
2024-05-03 07:37:34 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,26 +2,19 @@ import React from "react";
 
 //Stack Navigation
 import { NavigationContainer } from "@react-navigation/native"; //envolver a navegação da aplicação.
-import { createNativeStackNavigator } from "@react-navigation/native-stack"; //cria um navegador de pilha
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 // Isso permite que você use esse navegador de pilha para definir a navegação em sua aplicação.
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 //paginas
-import Home from "./src/pages/Home";
-import Copo from "./src/pages/Copo";
-import Tigela from "./src/pages/Tigela";
-import Acaiumlitro from "./src/pages/Acaiumlitro";
+import Routes from "./src/routes";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Copo" component={Copo} />
-        <Stack.Screen name="Tigela" component={Tigela} />
-        <Stack.Screen name="Acaiumlitro" component={Acaiumlitro} />
-      </Stack.Navigator>
+     <Routes />
     </NavigationContainer>
   );
 }
+
